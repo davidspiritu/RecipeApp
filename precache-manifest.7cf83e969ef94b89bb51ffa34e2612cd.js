42120584cc957854ce8fb58ef7b0b56ee65737ab
davidspiritu <davidspiritu@gmail.com>
2019-11-04 11:07:27 +0800

    Updates

---
+++ precache-manifest.7cf83e969ef94b89bb51ffa34e2612cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f588acb820a6eb2a0d132965b1408d",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "de51cf563a89b11bd161",
+    "url": "/RecipeApp/static/css/main.34e2aeda.chunk.css"
+  },
+  {
+    "revision": "fd154492630c1be20feb",
+    "url": "/RecipeApp/static/js/2.93e13f9d.chunk.js"
+  },
+  {
+    "revision": "de51cf563a89b11bd161",
+    "url": "/RecipeApp/static/js/main.a8d332f7.chunk.js"
+  },
+  {
+    "revision": "fd715a6352c0165647cf",
+    "url": "/RecipeApp/static/js/runtime-main.ccd6100e.js"
+  }
+]);
